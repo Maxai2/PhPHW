b2c4f6abd70f393d85d4c63cb2ae0238a9019a66
Хамелеон <maxali946@gmail.com>
2019-04-08 00:35:00 +0400

    init

---
+++ PHP_DZ_ru_week_1_1554279561/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class=wrap>
+        <div>
+            <label class="lbR">R:</label>
+            <input type="number" placeholder="0-255" max=255 min=0>
+        </div>
+
+        <div>
+            <label class="lbG">G:</label>
+            <input type="number" placeholder="0-255" max=255 min=0>
+        </div>
+
+        <div>
+            <label class="lbB">B:</label>
+            <input type="number" placeholder="0-255" max=255 min=0>
+        </div>
+    </div>
+
+    <?php
+        
+    ?>
+</body>
+</html>
