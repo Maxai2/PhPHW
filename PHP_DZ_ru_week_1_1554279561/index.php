35758f45ea7f0701727f1276177850d73da03c8c
Хамелеон <maxali946@gmail.com>
2019-04-08 18:14:50 +0400

    70%

--- PHP_DZ_ru_week_1_1554279561/index.php
+++ PHP_DZ_ru_week_1_1554279561/index.php
@@ -8,25 +8,56 @@
     <title>Document</title>
 </head>
 <body>
-    <div class=wrap>
-        <div>
-            <label class="lbR">R:</label>
-            <input type="number" placeholder="0-255" max=255 min=0>
+    <form class=wrap method="post" action="index.php">
+        <div class="groundContainer">
+            <div class="container">
+                <h3>Foreground</h3>
+
+                <div>
+                    <label class="lbR">R:</label>
+                    <input type="number" name="leftR"  placeholder="0-255" max=255 min=0>
+                </div>
+
+                <div>
+                    <label class="lbG">G:</label>
+                    <input type="number" name="leftG" placeholder="0-255" max=255 min=0>
+                </div>
+
+                <div>
+                    <label class="lbB">B:</label>
+                    <input type="number" name="leftB" placeholder="0-255" max=255 min=0>
+                </div>
+            </div>
+
+            <div class="container rightMargin">
+                <h3>Background</h3>
+
+                <div>
+                    <label class="lbR">R:</label>
+                    <input type="number" name="rightR" placeholder="0-255" max=255 min=0>
+                </div>
+
+                <div>
+                    <label class="lbG">G:</label>
+                    <input type="number" name="rightG" placeholder="0-255" max=255 min=0>
+                </div>
+
+                <div>
+                    <label class="lbB">B:</label>
+                    <input type="number" name="rightB" placeholder="0-255" max=255 min=0>
+                </div>
+            </div>
         </div>
 
-        <div>
-            <label class="lbG">G:</label>
-            <input type="number" placeholder="0-255" max=255 min=0>
+        <div class="spanContainer" id="spanBackground">
+            <span id="spanForeground">test</span>
         </div>
 
-        <div>
-            <label class="lbB">B:</label>
-            <input type="number" placeholder="0-255" max=255 min=0>
-        </div>
-    </div>
+        <input type="submit" value="Accept">
+    </form>
 
     <?php
-        
+        echo $_POST['leftR'] == '';
     ?>
 </body>
 </html>
