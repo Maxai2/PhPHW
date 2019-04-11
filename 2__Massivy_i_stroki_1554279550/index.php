1322c117b8e92397640cdd7b6335619e39f0b883
Хамелеон <maxali946@gmail.com>
2019-04-11 12:18:03 +0400

    numbers

--- 2__Massivy_i_stroki_1554279550/index.php
+++ 2__Massivy_i_stroki_1554279550/index.php
@@ -8,6 +8,70 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            $data = strip_tags($data);
+            // $data = mysqli_real_escape_string($data);
+            
+            return $data;
+        }
+
+        $number = $_POST['number'] ?? '';
+
+        $numberByLetter = array(
+            1 => array('ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'),
+            2 => array('десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать', 'двадцать', 'тридцать', 'сорок', 'пятьдесять', 'шестьдесять', 'семьдесять', 'восемьдесять', 'девяносто'),
+            3 => array('сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот')
+        );
+    ?>
+
+    <div class="wrap">
+        <form action="index.php" method="POST">
+            <div>
+                <span>Number by numeric:</span>
+                <input type="text" name="number">
+            </div>
+            <input type="submit" value="Convert">
+        </form>
+
+        <div class="result" style="display: <?php
+            if ($number == '') {
+                echo 'none';
+            } else {
+                echo 'flex';
+            }
+          ?>">
+
+            <span><?= $number; ?></span>
+            <span>
+                <?php 
+                    $number = strrev(test_input($number));
+                    // $numberCount = strlen($number);
+                    // $res = '';
+
+                    // while($numberCount != 0) {
+                    //     $num = $number % 10;
+
+                    //     if($num == 1 && )
+                    //     {
+
+                    //     }
+
+                    //     $res .= $numberByLetter[$numberCount][$num];
+                    //     $numberCount--;
+                    //     $number /= 10;
+                    // }
+
+                    $f = new NumberFormatter("ru", NumberFormatter::SPELLOUT);
+                    $res = $f->format($number);
+
+                    echo $res;
+                ?>
+            </span>
+        </div>
+    </div>
 </body>
 </html>
